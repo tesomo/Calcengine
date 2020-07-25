050ea15bd4beed70309116a9dfd94df8d50e0082
tesomo <60524644+tesomo@users.noreply.github.com>
2020-07-25 12:26:36 -0500

    Add files via upload

---
+++ src/com/tesomo/operators/Main.java
@@ -0,0 +1,24 @@
+package com.tesomo.operators;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int val1 = 7;
+        int val2 = 9;
+        int maxValue = val1 < val2 ? val1 : val2;
+
+        System.out.println(maxValue);
+        int a = 20;
+        int b = 15;
+        int c = 1;
+        if (a>b & b>c);
+        System.out.println("a and c are bigger than b");
+         if (a>b & c>b & c>a) System.out.println("c is the biggest number in the series");
+         else
+             System.out.println("not applicable");
+
+
+
+
+    }
+}
